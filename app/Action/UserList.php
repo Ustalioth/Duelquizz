a4eacbbf634be00d1493c3aeed50bcf5ed098e7d
RFrancois6 <romain57890@orange.fr>
2020-11-22 14:53:46 +0100

    add userlist

---
+++ app/Action/UserList.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Action;
+
+use App\Core\Controller\AbstractController;
+
+class UserList extends AbstractController
+{
+    public function __invoke()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (isset($_SESSION['isAdmin'])) {
+            if ($_SESSION['isAdmin'] === true) {
+                $connection = $this->getConnection();
+
+                $sql = 'select * from users';
+                $statement = $connection->query($sql);
+
+                $users = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+                return $this->render('users/list.html.twig', ['users' => $users]);
+            } else {
+                return $this->render('users/list.html.twig', ['msg' => 'Vous n\'êtes pas administrateur et ne pouvez donc pas acceder à cette page']);
+            }
+        }
+    }
+}
--- config/routing.php
+++ config/routing.php
@@ -8,12 +8,14 @@ use App\Action\Disconnect;
 use App\Action\Home;
 use App\Action\User;
 use App\Core\Routing\Route;
+use App\Action\UserList;
 
 return [
     new Route('/', Home::class, 'GET'),
     new Route('/user/{userName}', User::class, 'GET'),
 
     new Route('/admins', AdminList::class, 'GET'),
+    new Route('/users', UserList::class, 'GET'),
     new Route('/disconnect', Disconnect::class, 'GET'),
     new Route('/login', Login::class, ['GET', 'POST']),
     new Route('/admin[/{id}]', PersistAdmin::class, ['GET', 'POST']),
