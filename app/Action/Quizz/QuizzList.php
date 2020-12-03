436bde109a7bff10afbe448000d59c7ba29610ce
RFrancois6 <romain57890@orange.fr>
2020-12-03 19:34:39 +0100

    pagination

--- app/Action/Quizz/QuizzList.php
+++ app/Action/Quizz/QuizzList.php
@@ -11,16 +11,34 @@ class QuizzList extends AbstractController
 
         if (isset($_SESSION['isAdmin'])) {
             if ($_SESSION['isAdmin'] === true) {
+
+                    $currentPage =  1; 
+                
                 $connection = $this->getConnection();
                 $request = $this->getRequest();
                 $formParams = $request->getParsedBody();
 
+
                 if ($formParams !== null) {
-                    $sql = "SELECT DISTINCT * FROM users, quizzes WHERE
+                    $q = ("SELECT COUNT(quizzes.id) AS numberposts FROM users, quizzes WHERE
                     quizzes.user1 = users.id OR quizzes.user2 = users.id AND
-                    users.firstName LIKE '%:search%'  OR 
+                    users.firstName LIKE '%:search%'  OR
                     users.lastName LIKE '%:search%'  OR 
-                    users.email LIKE '%:search%'";
+                    users.email LIKE '%:search%'");
+                    
+                    $state = $connection->query($q);
+                    $data = $state->fetch(\PDO::FETCH_ASSOC);
+
+                    $number_posts= $data['numberposts'];
+
+                    $perPage = 20;
+                    $numberPages = ceil($number_posts/$perPage);
+
+                    $sql = ("SELECT * FROM users, quizzes WHERE
+                    quizzes.user1 = users.id OR quizzes.user2 = users.id AND
+                    users.firstName LIKE '%:search%'  OR
+                    users.lastName LIKE '%:search%'  OR 
+                    users.email LIKE '%:search%' LIMIT " . $perPage. " OFFSET " . ($currentPage-1)*$perPage); 
 
                     $statement = $connection->prepare($sql);
 
@@ -28,14 +46,26 @@ class QuizzList extends AbstractController
 
                     $statement->execute();
                 } else {
-                    $sql = 'select * from quizzes';
+                    $q = "SELECT COUNT(id) AS numberposts FROM quizzes";
+
+                    $state = $connection->query($q);
+                    $data = $state->fetch(\PDO::FETCH_ASSOC);
+
+                    $number_posts= $data['numberposts'];
+
+                    $perPage = 1;
+                    $numberPages = ceil($number_posts/$perPage);
+
+                    $sql = 'SELECT * FROM quizzes LIMIT ' . $perPage . " OFFSET " . ($currentPage-1)*$perPage;
+
                 }
                 $statement = $connection->prepare($sql);
 
                 if ($statement->execute()) {
                     $quizzes = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-                    return $this->render('quizzes/list.html.twig', ['quizzes' => $quizzes, 'isAdmin' => $_SESSION['isAdmin']]);
+                    var_dump($quizzes);die;
+                    return $this->render('quizzes/list.html.twig', ['quizzes' => $quizzes, 'isAdmin' => $_SESSION['isAdmin'], 'numberPages' => $numberPages]);
                 } else {
                     throw new \Exception('Quizz correspondant introuvable!');
                 }
