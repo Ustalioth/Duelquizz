3d377abfaaae83fc173821061ffe15a13c628541
RFrancois6 <romain57890@orange.fr>
2021-01-03 14:59:45 +0100

    fixed getThemes

--- app/Action/Theme/getThemes.php
+++ app/Action/Theme/getThemes.php
@@ -10,25 +10,25 @@ class getThemes extends AbstractController
 {
     public function __invoke()
     {
+        if ($this->getUser() !== null) {
+            $connexion = $this->getConnection();
+            $sth = $connexion->prepare("SELECT * FROM themes");
+            $sth->execute();
+            $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-        if($data['returnType'] === 'JSON'){
-            if ($this->getUser() !== null) {
-                $connexion = $this->getConnection();
-                $sth = $connexion->prepare("SELECT * FROM themes");
-                $sth->execute();
-                $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-    
+            if($data['returnType'] === 'JSON'){
+                $this->addHeader('Content-Type', 'application/json');
                 return json_encode([
                     "themes" => $result
                 ]);
-            } else {
-                throw new \LogicException('Token absent!');
             }
+            else{
+                $response = new Response($this->render('themes.xml.twig'));
+                $this->addHeader('Content-Type', 'application/xml');
+                return $response;   
+            }
+        } else {
+            throw new \LogicException('Token absent!');
         }
-        else{
-            $response = new Response($this->render('themes.xml.twig'));
-            $this->addHeader('Content-Type', 'application/xml');
-            return $response;   
-        }        
     }
 }
